7d4adf2d73ad448533d580ca5fa1566aff4a1328
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-05-15 00:55:08 +0000

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,19 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import RootLayout from "./pages/Root";
 import HomePage from "./pages/Home";
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+import EventsPage from "./pages/Events";
+import EventDetailPage from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+
+const router = createBrowserRouter([
+  { path: "/", element: <RootLayout /> },
+  { path: "/", element: <HomePage /> },
+  { path: "/events", element: <EventsPage /> },
+  { path: "/eventId", element: <EventDetailPage /> },
+  { path: "/new", element: <NewEventPage /> },
+  { path: "/:eventId/edit", element: <EditEventPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
