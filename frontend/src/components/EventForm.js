268de1ed5885ff3c0f38b20b62acc52d7e44709d
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-05-17 06:38:16 +0000

    update

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -5,8 +5,36 @@ function EventForm() {
     <form className={classes.form}>
       <p>
         <label htmlFor="title">Title</label>
-        <input type="text" id="title" />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          defaultValue="title"
+        />
       </p>
+      <p>
+        <label htmlFor="image">Image</label>
+        <input type="url" id="image" name="image" required defaultValue="url" />
+      </p>
+      <p>
+        <label htmlFor="date">Date</label>
+        <input type="date" id="date" name="date" required defaultValue="date" />
+      </p>
+      <p>
+        <label htmlFor="description">Desscription</label>
+        <textarea
+          id="description"
+          name="description"
+          rows="5"
+          required
+          defaultValue="description"
+        />
+      </p>
+      <div className={classes.actions}>
+        <button>Cancel</button>
+        <button>Save</button>
+      </div>
     </form>
   );
 }
