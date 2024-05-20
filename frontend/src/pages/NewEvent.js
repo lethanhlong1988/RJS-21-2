d8333329ec5bf85e0ab098203e5c92c703b617cf
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-05-20 02:33:59 +0000

    update

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,4 +1,22 @@
+import { redirect } from "react-router-dom";
+
+import EventForm from "../components/EventForm";
+
 function NewEventPage() {
-  return <h1>New Event Page</h1>;
+  return <EventForm />;
 }
 export default NewEventPage;
+
+export async function action({ request }) {
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+  console.log(eventData);
+  console.log("Chuan bi create New Event!!!");
+  return redirect("/events");
+}
