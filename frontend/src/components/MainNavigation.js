125dbf2d4a6ce9cb099e158b39b06c75484d6a5a
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-05-30 08:02:47 +0000

    update

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,8 +1,33 @@
+import { NavLink } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
 function MainNavigation() {
   return (
-    <div>
-      <div>MainNavigation</div>
-    </div>
+    <header>
+      <nav>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+            end
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/events"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+          >
+            Events
+          </NavLink>
+        </li>
+      </nav>
+    </header>
   );
 }
 export default MainNavigation;
