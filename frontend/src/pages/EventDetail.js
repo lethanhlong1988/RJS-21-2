01372a0512c7dbfe45a152326e04352e99aeff21
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-06-03 08:49:45 +0000

    update

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,7 +1,10 @@
-import { useParams } from "react-router-dom";
+import { useParams, json, useLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 function EventDetailPage() {
   const params = useParams();
+  const data = useLoaderData();
+  console.log(data.event);
+
   return (
     <div>
       <div>Event Detail Page</div>
@@ -18,6 +21,10 @@ export async function loader({ request, params }) {
   const response = await fetch("http://localhost:8080/events/" + id);
   if (!response.ok) {
     // Thong bao va su ly loi
+    throw json(
+      { message: "Could not fetch details for selected event." },
+      { status: 500 },
+    );
   } else {
     return response;
   }
